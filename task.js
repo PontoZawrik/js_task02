c45cb97fad04a372f133aeffc1b02766bb950b29
PontoZawrik <Psichotroniks@tut.by>
2023-12-03 14:50:50 +0300

    task01 - task02

---
+++ task.js
@@ -0,0 +1,46 @@
+// 1. Запросить у пользователя его возраст и определить, кем он
+// является: ребенком (0–2), подростком (12–18), взрослым
+// (18_60) или пенсионером (60– ...).
+// //
+// const age = Number(prompt("Введите свой возраст: "))
+//
+// if (age >= 0 && age <= 12) {
+//     alert("Ребенок")
+// }
+// else if (age > 12 && age < 18) {
+//     alert("Подросток")
+// }
+// else if (age >= 18 && age <= 60) {
+//     alert("Взрослый")
+// }
+// else if (age > 60 && age <= 120) {
+//     alert("Пенсионер")
+// }
+// else {
+//     alert("Error")
+// }
+
+
+// 2. Запросить у пользователя число от 0 до 9 и вывести ему
+// спецсимвол, который расположен на этой клавише (1–!,
+// 2–@, 3–# и т. д).
+//
+// const number = Number(prompt("Введите число (0 - 9): "))
+//
+// let result;
+// switch(number) {
+//     case 1: result = "!"; break;
+//     case 2: result = "@"; break;
+//     case 3: result = "#"; break;
+//     case 4: result = "$"; break;
+//     case 5: result = "%"; break;
+//     case 6: result = "^"; break;
+//     case 7: result = "&"; break;
+//     case 8: result = "*"; break;
+//     case 9: result = "("; break;
+//     case 0: result = ")"; break;
+//     default: result = "Error"
+// }
+// alert(result)
+
+
